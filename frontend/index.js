91eeb41eee0ff2a6e03ee0885b8d82b5f30e99fc
mororor <matthias@syndikat.org>
2019-07-30 22:43:00 +0200

    mini frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import ReactDOM from 'react-dom'
+import ndjson from '../util/ndjson-duplex-stream'
+import ws from 'websocket-stream'
 
+const baseUrl = 'ws://localhost:9191'
+  
 function Page () {
-  return <p>Hello World</p>
+  const [results, setResults] = useState([])
+  useMemo(() => { useQuery(results, setResults)}, [])
+  return <div>
+    <p>H</p>
+    <p>{results}</p>
+  </div>
+}
+
+function useQuery (data, setData) {
+  const coll = []
+  const websocket = ws(baseUrl + '/query/entities.all')
+  const stream = ndjson(websocket)
+  stream.on('data', row => {
+    console.log('query', row)
+    coll.push(row)
+  })
+  stream.on('error', err => {
+    console.error('error', err)
+  })
+  stream.on('end', () => {
+    setData(JSON.stringify(coll))
+  })
+  return data
 }
 
 ReactDOM.render(<Page />, document.getElementById('root'))
+
+
